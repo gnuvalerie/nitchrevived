5434cd8fd6b2706aa1af19d165fa5c9b399414b2
tetoparty <git@noreply.teto.party>
2025-09-19 18:45:33 +0200

    wrong font

--- asciilogo.py
+++ asciilogo.py
@@ -1,6 +1,6 @@
 import pyfiglet
 import os
 text = input()
-f = pyfiglet.figlet_format(text, font="slant")
+f = pyfiglet.figlet_format(text, font="smslant")
 with open(os.path.expanduser("~temp.txt"), "w") as file:
     file.write(f)
