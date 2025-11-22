3dd3ae0a3ff5da5cb33ac93b99a824c590616269
tetoparty <git@noreply.teto.party>
2025-11-22 16:38:37 +0100

    bugfix

--- src/funcs/getFlatpakInstalled.nim
+++ src/funcs/getFlatpakInstalled.nim
@@ -1,7 +1,7 @@
 import std/[os, osproc]
 
 proc getFlatpakInstalled*(): bool =
-    if osproc.execCmdEx("flatpak --version").exitcode == 0:
+    if osproc.execCmdEx("flatpak --version").exitCode == 0:
         result = true
     else:
         result = false
